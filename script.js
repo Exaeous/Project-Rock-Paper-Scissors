fabec3054710dac8515c0109ff45e23ba572fdd1
Samuel <SamuelPulukuri@outlook.com>
2024-05-15 19:10:17 +0530

    Add computer selection logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use script";
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+const randomChoices = Math.floor(Math.random() * choices.length);
+
+const computerChoice = choices[randomChoices];
+
+function getComputerChoice() {
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
