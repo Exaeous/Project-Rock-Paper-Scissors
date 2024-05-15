c80667a8e9436f7b037e842c2db65bc9d09928cf
Samuel <SamuelPulukuri@outlook.com>
2024-05-15 19:39:01 +0530

    Add human selection logic and change choice case

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 "use script";
 
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 const randomChoices = Math.floor(Math.random() * choices.length);
 
 const computerChoice = choices[randomChoices];
+
+let humanChoice = prompt("Select your choice");
+
+humanChoice = humanChoice.toUpperCase();
 
 function getComputerChoice() {
   return computerChoice;
 }
 
 console.log(getComputerChoice());
+
+function getHumanhoice() {
+  return humanChoice;
+}
+
+console.log(getHumanhoice());
