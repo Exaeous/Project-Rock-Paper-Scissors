087aa7dfbb8bb938ad21788cfe36e3e71b09ac8a
Sam <120003855+Exaeous@users.noreply.github.com>
2026-03-03 22:58:45 +0530

    Add logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function getComputerChoice() {
+  let randomChoice = Math.random();
+  let computerChoice = "";
+  if (randomChoice > 0 && randomChoice <= 1 / 3) {
+    computerChoice = "Rock";
+  } else if (randomChoice > 1 / 3 && randomChoice <= 2 / 3) {
+    computerChoice = "Paper";
+  } else if (randomChoice > 2 / 3 && randomChoice < 1) {
+    computerChoice = "Scissors";
+  }
+  console.log(computerChoice);
+}
+
+getComputerChoice();
