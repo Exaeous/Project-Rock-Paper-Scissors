6a4cae987410e68754c6ce98407568571b48a8f2
Sam <120003855+Exaeous@users.noreply.github.com>
2026-03-05 00:48:48 +0530

    Add logic to play a single round of the game

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let computerScore = 0;
 
 function getHumanChoice() {
   let humanChoice = prompt("Enter your choice:");
-  console.log(humanChoice);
+  return humanChoice;
 }
 
 function getComputerChoice() {
   let randomChoice = Math.random();
-  let computerChoice = "";
+  let computerChoice = " ";
   if (randomChoice > 0 && randomChoice <= 1 / 3) {
     computerChoice = "Rock";
   } else if (randomChoice > 1 / 3 && randomChoice <= 2 / 3) {
@@ -18,8 +18,36 @@ function getComputerChoice() {
   } else if (randomChoice > 2 / 3 && randomChoice < 1) {
     computerChoice = "Scissors";
   }
-  console.log(computerChoice);
+  return computerChoice;
 }
 
-getHumanChoice();
-getComputerChoice();
+function playRound(humanChoice, computerChoice) {
+  let finalMessage = " ";
+  if (humanChoice === "Rock" && computerChoice === "Paper") {
+    finalMessage = "You lose! Paper beats Rock";
+    computerScore += 1;
+  } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+    finalMessage = "You lose! Scissors beats Paper";
+    computerScore += 1;
+  } else if (humanChoice === "Scissors " && computerChoice === "Rock") {
+    finalMessage = "You lose! Rock beats Scissors";
+    computerScore += 1;
+  } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+    finalMessage = "You win! Paper beats Rock";
+    humanScore += 1;
+  } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+    finalMessage = "You win! Scissors beats Paper";
+    humanScore += 1;
+  } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+    finalMessage = "You win! Rock beats Scissors";
+    humanScore += 1;
+  } else {
+    finalMessage = "It's a draw";
+  }
+  return finalMessage;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
